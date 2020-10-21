ab05a47601d464df6fa2ee439a2a4d5ae2a071a1
Seol Cheon <jojo432143@gmail.com>
2020-10-21 15:51:09 -0400

    displaying login page but error occured..

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let router  = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// define the User Model instance
+let userModel = require('../models/user');
+let User = userModel.User; //allias
 
 module.exports.displayHomePage = (req, res, next) => {
     res.render('index', {title: 'Home'});
@@ -20,3 +26,110 @@ module.exports.displayServicesPage = (req, res, next) => {
 module.exports.displayContactPage = (req, res, next) => {
     res.render('index', {title: 'Contact'});
 }
+
+module.exports.displayLoginPage = (req, res, next) => {
+    //check if the user if already logged in
+    if(!req.user)
+    {
+        res.render('../views/auth/login',
+        {
+            title: "Login",
+            messages: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        })
+    }
+    else
+    {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processLoginPage = (req, res, next) =>{
+    passport.authenticate('local',
+    (err, user, info) =>{
+        //server error?
+        if(err)
+        {
+            return next(err);
+        }
+        //is there a user login error?
+        if(!user)
+        {
+            req.flash('loginMessage', 'Authentication Error');
+            return res.redirect('/login');
+        }
+        req.login(user, (err) =>{
+            //server error?
+            if(err)
+            {
+                return next(err);
+            }
+            return res.redirect('/contact-list');
+        });
+    })(req, res, next);
+}
+
+module.exports.performLogout = (req, res, next) =>{
+    req.logout();
+    res.redirect('/');
+}
+
+/* for register page */
+module.exports.displayRegisterPage = (req, res, next) => {
+    //check if the user is not already logged in
+    if(!req.user)
+    {
+        res.render('../views/auth/register',
+        {
+            title: 'Register',
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName: ''
+        });
+    }
+    else
+    {
+        return res.redirect('/')
+    }
+}
+
+module.exports.processRegisterPage = (req, res, next) => {
+    //instatiate a user object
+    let newUser = new User({
+        username : req.body.username,
+        //password: requ.body.password
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.password, (err) =>{
+        if(err)
+        {
+            console.log("Error: Inserting New User");
+            if(err.name == "UserExistsError")
+            {
+                req.flash(
+                    'registerMessage',
+                    'Registration Error: User Already Exists!'
+                );
+                console.log('Error: User Already Exists!')
+            }
+            return res.render('../views/auth/register',
+            {
+                title: 'Register',
+                messages: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        }
+        else
+        {
+            //if no error exists, then registration is successful
+            //redirect the user and authenticate them
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/contact-list')
+            });
+        }
+    });
+}
+
+
+
--- server/routes/businessContact.js
+++ server/routes/businessContact.js
@@ -2,26 +2,37 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-//connect to our Contact Model
-let Contacts = require('../models/businessContact');
+let passport = require('passport');
 
 let contactController = require('../controllers/businessContact');
 
+//helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    //check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 /* GET Route for the Contact List page - READ Operation */
-router.get('/', contactController.displayContactList);
+router.get('/', requireAuth, contactController.displayContactList);
 
 /* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', contactController.displayAddPage);
+router.get('/add', requireAuth, contactController.displayAddPage);
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', contactController.processAddPage);
+router.post('/add', requireAuth, contactController.processAddPage);
 
 /* GET Route for displaying the Edit page - UPDATE Operation */
-router.get('/edit/:id', contactController.displayEditPage);
+router.get('/edit/:id', requireAuth, contactController.displayEditPage);
 
 /* POST Route for processing the Edit page - UPDATE Operation */
-router.post('/edit/:id', contactController.processEditPage);
+router.post('/edit/:id', requireAuth, contactController.processEditPage);
 
 /* GET to perform Contact Deletion - DELETE Operation */
-router.get('/delete/:id', contactController.performDelete );
+router.get('/delete/:id', requireAuth, contactController.performDelete );
+
 module.exports = router;
