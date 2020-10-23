5c08b8574491551664a7a5d3d7a79f36371b130b
Seol Cheon <jojo432143@gmail.com>
2020-10-22 21:07:07 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_store"
+    //"URI": "mongodb://localhost/business_store"
+    "URI": "mongodb+srv://seollen:XFG67PrN6Ijx82Pq@mongodbserver.mmq4s.mongodb.net/business_store?retryWrites=true&w=majority"
 
 }
